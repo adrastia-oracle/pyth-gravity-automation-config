dcbdb6ddc2fb7f6b7bc0ce4b4ec4d01fd0415bfc
Tyler Loewen <tyler@trilez.com>
2024-10-20 22:45:59 -0700

    Increase tx confirmation timeout from 2s to 5s

--- worker-config.ts
+++ worker-config.ts
@@ -91,8 +91,8 @@ const config: AdrastiaConfig = {
                 gasPriceMultiplierDivisor: 100n,
                 // Check for tx confirmations every 250ms
                 confirmationPollingInterval: 250,
-                // Wait up to 2 seconds for tx confirmations
-                transactionConfirmationTimeout: 2_000,
+                // Wait up to 5 seconds for tx confirmations
+                transactionConfirmationTimeout: 5_000,
                 // Wait for 5 confirmations
                 waitForConfirmations: 5,
             },
